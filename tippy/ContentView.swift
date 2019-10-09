992e3bf1adf529be46171da8d0a70fbe8c4f357f
Sasri Dedigama <sasridedigama97@gmail.com>
2019-10-08 23:34:27 -0700

    cal

---
+++ tippy/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  tippy
+//
+//  Created by Sasri Dedigama on 10/8/19.
+//  Copyright © 2019 Sasri Dedigama. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+  
